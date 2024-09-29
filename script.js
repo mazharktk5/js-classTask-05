113a98a437a917cfc9c1d805c38b7b0d119dac28
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-29 00:31:53 -0700

    updated the javscript code to get the values of years months and days

--- script.js
+++ script.js
@@ -4,19 +4,33 @@ function calculateAge() {
     var birthday = document.getElementById("dob").value
 var years = document.getElementById("years")
 var months = document.getElementById("months")
-var days = document.getElementById("Days")
+var days = document.getElementById("days")
 
 if(birthday){
     var today = new Date();
     var birthDate = new Date(birthday);
     var year = today.getFullYear() - birthDate.getFullYear();
     var month = today.getMonth() - birthDate.getMonth();
-    var day = today.getDay() - birthDate.getDay();
+    var day = today.getDate() - birthDate.getDate();
+    
+    if (day < 0) {
+        month--;
+        day += new Date(year, month, 0).getDate();
+    }
+    
+    if (month < 0) {
+        year--;
+        month += 12;
+    }
+    
+    years.textContent = year;
+    months.textContent = month;
+    days.textContent = day;
+    }else{
+    alert("Enter a valid date");
+    }
 
 
-
-}else{
-    alert("Enter your birth date")
 }
 
-}
+
