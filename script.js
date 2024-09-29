6d61928bf8e0930e518e47d1e9d7946696022585
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-28 23:53:01 -0700

    write fucntion to calculate age

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function calculateAge() {
+    var birthday = document.getElementById("dob").value
+var result = document.getElementById("ageResult")
+if(birthday){
+   
+
+    var today = new Date();
+    var birthDate = new Date(birthday);
+    var year = today.getFullYear() - birthDate.getFullYear();
+    var month = today.getMonth() - birthDate.getMonth();
+    var day = today.getDay() - birthDate.getDay();
+}
+
+}
